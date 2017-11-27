46986ba5b5c5a289cfc9a442c1d3f9d3348bd0d8
kaynewbie1 <tenniscrazy1@gmail.com>
2017-11-27 13:02:46 -0600

    edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   protect_from_forgery with: :exception
 
   add_flash_types :danger, :info, :warning, :success
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "You have logged in"
-
       redirect_to root_path
     else
       flash[:notice] = "Your username or password is incorret"
